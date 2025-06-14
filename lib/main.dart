488eb39a7a5c8600e342bc521ff9719ab15d582a
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-06-14 02:02:53 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:cinemapedia/config/router/app_router.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
+
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +13,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return  MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      
     );
   }
 }
+
+
