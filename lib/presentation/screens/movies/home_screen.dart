b2e6d80f78c4276a2cf25995784980ef78359715
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-06-15 02:04:40 -0500

    create swiper

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -37,20 +37,13 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     return Column(
       children: [
 
-        CustomAppbar(),
+        const CustomAppbar(),
+
+        MoviesSlideshow(movies: nowPlayingMovies)
 
 
 
-        Expanded(
-          child: ListView.builder(
-              itemCount: nowPlayingMovies.length,
-              itemBuilder: (context, index) {
-                final movie = nowPlayingMovies[index];
-                return ListTile(
-                  title: Text(movie.title),
-                );
-              }),
-        )
+        
       ],
     );
   }
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,1 +1,3 @@
+export 'package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart';
+
 export 'shared/custom_appbar.dart';
