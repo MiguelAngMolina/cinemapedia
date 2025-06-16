2211aa1ebb46d39dbdcba0b7f2470b27e611ac5a
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-06-15 23:10:01 -0500

    navigating to new screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,19 +1,24 @@
-import 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
+import '../../presentation/screens/screens.dart';
 
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context,state) => const HomeScreen(),
-      ),
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+    routes: [
 
+      GoRoute(
+      path: 'movie/:id',
+      name: MovieScreen.name,
+      builder: (context, state) {
+        final movieId = state.pathParameters['id'] ?? 'no-id';
+
+        return MovieScreen(movieId: movieId);
+      }),
       
-
-
-]
-
-);
+    ]
+  ),
+  
+]);
