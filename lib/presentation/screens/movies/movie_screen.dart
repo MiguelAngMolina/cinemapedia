bf69ae9abf936d56d29ec3effd71cb1109c8a579
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-06-16 03:30:55 -0500

    show actors

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -60,39 +60,39 @@ class _MovieDetails extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     final textStyles = Theme.of(context).textTheme;
 
-
-
-    return  Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(padding: EdgeInsets.all(8),
-        child: Row(
-          
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(
-                movie.posterPath,
-                width: size.width *0.3,
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
                 ),
-            ),
-
-            const SizedBox(width: 10,),
-
-            SizedBox(
-              width: (size.width -40) * 0.7,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(movie.title, style: textStyles.titleLarge,),
-                  Text(movie.overview)
-                ],
               ),
-            )
-          ],
-
-        ),
+              const SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                width: (size.width - 40) * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: textStyles.titleLarge,
+                    ),
+                    Text(movie.overview)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
 
         //Generos de la pelicula
@@ -101,22 +101,84 @@ class _MovieDetails extends StatelessWidget {
           child: Wrap(
             children: [
               ...movie.genreIds.map((e) => Container(
-                margin: const EdgeInsets.only(right: 10),
-                child: Chip(
-                  label: Text(e),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                ),
-              ))
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Chip(
+                      label: Text(e),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ))
             ],
           ),
-          
-          )
-,
+        ),
 
-      
         //TODO: Mostrar actores
-        const SizedBox(height: 100,)
+        _ActorsByMovie(
+          movieId: movie.id.toString(),
+        ),
+
+        const SizedBox(
+          height: 50,
+        )
       ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+  final String movieId;
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final actorsByMovie = ref.watch(actorsByMovieProvider);
+
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(
+        strokeWidth: 2,
+      );
+    }
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                // actor phto
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    fit: BoxFit.cover,
+                    height: 180,
+                    width: 135,
+                  ),
+                ),
+
+                const SizedBox(height: 5,),
+                Text(actor.name , maxLines: 2,),
+                Text(actor.character ?? ' ',
+                 maxLines: 2, 
+                 style: const TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),)
+
+
+                //name
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -136,11 +198,11 @@ class _CustomSliverAppbar extends StatelessWidget {
         foregroundColor: Colors.white,
         flexibleSpace: FlexibleSpaceBar(
           titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          title: Text(
-            movie.title,
-            style: const TextStyle(color: Colors.white, fontSize: 20),
-            textAlign: TextAlign.start,
-          ),
+          // title: Text(
+          //   movie.title,
+          //   style: const TextStyle(color: Colors.white, fontSize: 20),
+          //   textAlign: TextAlign.start,
+          // ),
           background: Stack(
             children: [
               SizedBox.expand(
