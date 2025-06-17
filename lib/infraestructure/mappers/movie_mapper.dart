873abc9b4f853219a08dcb44c5b681847505bc5b
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-06-17 01:23:44 -0500

    show suggestions

--- lib/infraestructure/mappers/movie_mapper.dart
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -16,7 +16,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
           ? 'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-          : 'no-poster',
+          : 'https://i.pinimg.com/564x/9b/c4/d0/9bc4d087682c1cf9beb9669bad13cc3c.jpg',
       releaseDate: moviedb.releaseDate ,
       title: moviedb.title,
       video: moviedb.video,
