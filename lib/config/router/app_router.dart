8a7bfbdafb387e46a33aa52d5a704d18627090bc
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-06-18 01:04:10 -0500

    shell router config

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,22 +4,55 @@ import 'package:go_router/go_router.dart';
 import '../../presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-    path: '/',
-    name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(childView: FavoriteViews(),),
-    routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
 
-      GoRoute(
-      path: 'movie/:id',
-      name: MovieScreen.name,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              }),
+          ]
+        ),
 
-        return MovieScreen(movieId: movieId);
-      }),
-      
-    ]
-  ),
-  
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoriteViews();
+          },
+        )
+
+
+
+      ]),
+
+  // Rutas padre e hijo
+  // GoRoute(
+  //   path: '/',
+  //   name: HomeScreen.name,
+  //   builder: (context, state) => const HomeScreen(childView: FavoriteViews(),),
+  //   routes: [
+
+  //     GoRoute(
+  //     path: 'movie/:id',
+  //     name: MovieScreen.name,
+  //     builder: (context, state) {
+  //       final movieId = state.pathParameters['id'] ?? 'no-id';
+
+  //       return MovieScreen(movieId: movieId);
+  //     }),
+
+  //   ]
+  // ),
 ]);
