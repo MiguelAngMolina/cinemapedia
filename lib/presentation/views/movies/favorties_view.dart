36a4c324852ef7548223c28bc85f43c0f00ed6ad
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-08-25 01:34:46 -0500

    loading favorite movies title

--- lib/presentation/views/movies/favorties_view.dart
+++ lib/presentation/views/movies/favorties_view.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavortiesView extends StatelessWidget {
+import '../../providers/providers.dart';
+
+class FavortiesView extends ConsumerStatefulWidget {
   const FavortiesView({super.key});
 
   @override
+  FavortiesViewState createState() => FavortiesViewState();
+}
+
+class FavortiesViewState extends ConsumerState<FavortiesView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites'),
-      ),
-      body: const Center(
-        child: Text('Favorites'),
-      ),
+      body: ListView.builder(
+          itemCount: favoriteMovies.length,
+          itemBuilder: (context, index) {
+            final movie = favoriteMovies[index];
+
+            return ListTile(
+              title: Text(movie.title));
+          }),
     );
   }
 }
