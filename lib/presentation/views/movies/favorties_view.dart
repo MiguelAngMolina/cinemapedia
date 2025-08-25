d1b9a8bbd2f69e34db7d430e4ecdcb3a112dd332
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-08-25 02:46:00 -0500

    add message "No favorite movies" screen

--- lib/presentation/views/movies/favorties_view.dart
+++ lib/presentation/views/movies/favorties_view.dart
@@ -1,6 +1,7 @@
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../providers/providers.dart';
 
@@ -36,8 +37,28 @@ class FavortiesViewState extends ConsumerState<FavortiesView> {
   Widget build(BuildContext context) {
     final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
 
-    return MovieMasonry(
-      loadNextPage: loadNextPage,
-      movies: favoriteMovies);
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_outline_sharp, size: 60, color: colors.primary,),
+            Text('Oh no!!', style: TextStyle(fontSize: 30, color: colors.primary),),
+             Text('No hay nashes agregados como favos OwO', style: TextStyle(fontSize: 15, color: colors.secondary),),
+
+
+             const SizedBox(height: 20,),
+              FilledButton.tonal(onPressed: ()=> context.go('/home/0'),
+               child: const Text('Vuelve a casa'))
+
+
+          ],
+        ),
+      );
+    }
+
+    return MovieMasonry(loadNextPage: loadNextPage, movies: favoriteMovies);
   }
 }
