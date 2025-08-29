c72fe046aa4bae144425d27488c478dc880f84ec
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-08-28 22:34:27 -0500

    page view

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -3,10 +3,34 @@ import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget    {
   static const name = 'home-screen';
   final int pageIndex;
   const HomeScreen({super.key, required this.pageIndex});
+
+@override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+//* Este Mixin es necesario para mantener el estado en el PageView
+class _HomeScreenState extends State<HomeScreen> with AutomaticKeepAliveClientMixin {
+
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(
+      keepPage: true
+    );
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
 
   final viewRoutes = const <Widget>[
     HomeView(),
@@ -16,12 +40,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+        super.build(context);
+
+    if ( pageController.hasClients ) {
+      pageController.animateToPage(
+        widget.pageIndex, 
+        curve: Curves.easeOut, 
+        duration: const Duration( milliseconds: 200)
+      );
+    }
+
     return  Scaffold(
-      body: IndexedStack(
-        index: pageIndex,
+      body: PageView(
+        controller: pageController,
+        // index: pageIndex,
         children: viewRoutes,
       ),
-      bottomNavigationBar: CustomBottomNavigationbar(pageIndex: pageIndex,),
-    );
+        bottomNavigationBar: CustomBottomNavigationbar(
+          pageIndex: widget.pageIndex,
+      ),    );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
